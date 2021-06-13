32f7e2337d2e3ef750a1c173685590f0acf249bd
manemejeff <kmb14056@yandex.ru>
2021-06-13 16:33:31 +0400

    tutorial 3 added detail.html, index.html some views and url routing

--- comrad_major/comrad_major/settings.py
+++ comrad_major/comrad_major/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'questions',
     'interrogations',
     'results',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'comrad_major.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates/')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
