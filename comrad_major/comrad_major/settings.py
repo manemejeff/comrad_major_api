8466e447ca790dd0d1a9c45501c8c95b897342f7
manemejeff <kmb14056@yandex.ru>
2021-06-14 17:55:52 +0400

    tutorial 7 admin

--- comrad_major/comrad_major/settings.py
+++ comrad_major/comrad_major/settings.py
@@ -126,11 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATICFILES_DIRS = [
-
+    BASE_DIR / 'static/',
 ]
 
 # Default primary key field type
--- comrad_major/polls/admin.py
+++ comrad_major/polls/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 
 from .models import PollQuestion, PollChoice
 
+
 # Register your models here.
 
-admin.site.register(PollQuestion)
+class PollChoiceInline(admin.TabularInline):
+    model = PollChoice
+    extra = 3
+
+
+class PollChoiceAdmin(admin.ModelAdmin):
+    pass
+
+
+class PollQuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [PollChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
+admin.site.register(PollQuestion, PollQuestionAdmin)
 admin.site.register(PollChoice)
